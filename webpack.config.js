8c1e5113af8f9c253b9ef6864755d01e7a87a45d
wengeek <wenwei897684475@gmail.com>
2015-07-02 00:30:15 +0800

    change package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ var path = require('path');
 var node_modules = path.resolve(__dirname, 'node_modules');
 var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 
-
 module.exports = {
     entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/main.js')],
     output: {
